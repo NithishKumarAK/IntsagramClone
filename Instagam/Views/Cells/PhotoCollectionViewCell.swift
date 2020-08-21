87b98676366078c15808e4af09e22ec3467ba960
Akhadjon Abdukhalilov <abduxalilovahadjon97@gmail.com>
2020-08-22 04:31:11 +0500

    Profile photos grid

--- Instagam/Views/Cells/PhotoCollectionViewCell.swift
+++ Instagam/Views/Cells/PhotoCollectionViewCell.swift
@@ -27,6 +27,21 @@ class PhotoCollectionViewCell: UICollectionViewCell {
        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: radius).cgPath
        layer.cornerRadius = radius
    }
+    func shadowlDecorate() {
+        let radius: CGFloat = 10
+        contentView.layer.cornerRadius = radius
+        contentView.layer.borderWidth = 1
+        contentView.layer.borderColor = UIColor.clear.cgColor
+        contentView.layer.masksToBounds = true
+
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 1.0)
+        layer.shadowRadius = 2.0
+        layer.shadowOpacity = 0.5
+        layer.masksToBounds = false
+        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: radius).cgPath
+        layer.cornerRadius = radius
+    }
     
     
 }
